c5f058337a7be113257db192244a655bd87c1b86
Xiao Cui <64198371+sacredcodex@users.noreply.github.com>
2024-04-25 20:21:26 +1000

    Update model.py
    修复模型设置保存的错误

--- model.py
+++ model.py
@@ -6,9 +6,9 @@ def create_model(name="知识查询助手",type="abab5.5s-chat", ttg=256, temp=0
         'name' : name,
         'description' : f"{name}是一款由MiniMax自研的，没有调用其他产品的接口的大型语言模型。MiniMax是一家中国科技公司，一直致力于进行大模型相关的研究。",
         'model_type' : type,
-        'tokens_to_generate' : ttg,
-        'temperature' : temp,
-        'top_p' : topp
+        'tokens_to_generate' : int(ttg),
+        'temperature' : float(temp),
+        'top_p' : float(topp)
     }
     print(model)
     with open(f'{name}.json', 'w+') as file:
@@ -17,7 +17,7 @@ def create_model(name="知识查询助手",type="abab5.5s-chat", ttg=256, temp=0
 if __name__ == '__main__':
     name = input("模型名字(文件名\称呼)：")
     type = input("模型类型(abab6-chat, abab5.5-chat, abab5.5s-chat, 以上3选1):")
-    ttq = input("模型生成上限:")
+    ttg = input("模型生成上限:")
     temp = input("temperature(0~1,越高创造性越强，越低准确性越强):")
     topp = input("top_p(越大越随机):")
-    create_model()
+    create_model(name, type, ttg, temp, topp)
